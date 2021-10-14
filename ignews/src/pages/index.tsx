eb4aec8e476e2eafc35a317c3c023a13ee0fdab2
Fernando <fernando.vgarcia@hotmail.com>
2021-10-13 22:41:14 -0300

    Global styles added.

--- ignews/src/pages/index.tsx
+++ ignews/src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from 'next/head';
 
 export default function Home() {
   return (
-    <h1>Hello world</h1>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+    </>
   )
 }
